1ff29f0ac19dadb684ceee0629a5992eebf21bc5
rinz <roberts.denisovs@gmail.com>
2024-06-08 14:03:41 +0300

    aaa

--- app/TransitionManager.tsx
+++ app/TransitionManager.tsx
@@ -3,43 +3,46 @@ import { PostProcessing } from "./PostProcessing";
 
 const memoryState: any = {};
 
-function useMemoryState(key: any, initialState: any){
+function useMemoryState(key: any, initialState: any) {
     const [state, setState] = useState(() => {
-      const hasMemoryValue = Object.prototype.hasOwnProperty.call(memoryState, key);
-      if (hasMemoryValue) {
-        return memoryState[key]
-      } else {
-        return typeof initialState === 'function' ? initialState() : initialState;
-      }
+        const hasMemoryValue = Object.prototype.hasOwnProperty.call(memoryState, key);
+        if (hasMemoryValue) {
+            return memoryState[key]
+        } else {
+            return typeof initialState === 'function' ? initialState() : initialState;
+        }
     });
-  
+
     function onChange(nextState: any) {
-      memoryState[key] = nextState;
-      setState(nextState);
+        memoryState[key] = nextState;
+        setState(nextState);
     }
-  
+
     return [state, onChange];
-  }
+}
 
 export default function TransitionManager({ children }: Readonly<{ children: React.ReactNode }>) {
-    const [displayChildren, setDisplayChildren] = useMemoryState('children', children)
+    const [displayChildren, setDisplayChildren] = useMemoryState('children', null)
     const [test, setTest] = useMemoryState('text', 'old')
     const [transitionStage, setTransitionStage] = useMemoryState('transitionStage', ['fadeIn'])
     console.log(transitionStage)
 
     useEffect(() => {
         console.log(transitionStage)
-        
-        
+
         if (children !== displayChildren) {
             console.log(test)
             setTest('new')
             setTransitionStage("fadeOut")
         }
-        else
-        {
+        else {
             console.log("CHILDREN ARE SAME")
         }
+
+        if (test == "old") {
+            setDisplayChildren(children)
+        }
+
     }, [children, setDisplayChildren, displayChildren])
 
     return (
