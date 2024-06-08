2d221d74ffb749d331fcf181303f76e5f5596d11
rinz <roberts.denisovs@gmail.com>
2024-06-08 13:47:36 +0300

    im not wasting another day on this

--- app/TransitionManager.tsx
+++ app/TransitionManager.tsx
@@ -44,6 +44,7 @@ export default function TransitionManager({ children }: Readonly<{ children: Rea
         <>
             <PostProcessing transitionStage={transitionStage} />
             {children}
+            {displayChildren}
         </>
     )
 }
